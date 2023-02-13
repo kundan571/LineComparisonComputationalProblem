69c0fe3ed9920228a760a4831960d1790b587882
kundan571 <kundanchauhan1997@gmail.com>
2023-02-13 23:57:42 +0530

    Checking equality of lines

--- src/linecomparisonproblem/Linecomparisonucbuilder.java
+++ src/linecomparisonproblem/Linecomparisonucbuilder.java
@@ -1,5 +1,4 @@
 package linecomparisonproblem;
-
 import java.util.Scanner;
 public class Linecomparisonucbuilder {
     // Calculate length and return
@@ -11,14 +10,31 @@ public class Linecomparisonucbuilder {
         Scanner input = new Scanner(System.in);
         Linecomparisonucbuilder length = new Linecomparisonucbuilder();
         int x1, y1, x2, y2;
+        int x3, y3, x4, y4;
+        // For first line
         System.out.println("Enter first coordinates x1 and y1: ");
         x1 = input.nextInt();
         y1 = input.nextInt();
         System.out.println("Enter second coordinates x2 and y2: ");
         x2 = input.nextInt();
         y2 = input.nextInt();
-        System.out.println("length of line: " + length.lengthCalculate(x1, y1, x2, y2));
-
+        // for second line
+        System.out.println("Enter coordinates of second line");
+        System.out.println("Enter first coordinates x3 and y3: ");
+        x3 = input.nextInt();
+        y3 = input.nextInt();
+        System.out.println("Enter second coordinates x4 and y4:");
+        x4 = input.nextInt();
+        y4 = input.nextInt();
+        //invoking method
+        Integer line1 = length.lengthCalculate(x1, y1, x2, y2);
+        Integer line2 = length.lengthCalculate(x3, y3, x4, y4);
+        // Checking equality of lines
+        if (line1.equals(line2)) {
+            System.out.println("Both lines are equal");
+        } else {
+            System.out.println("Both lines are not equal");
+        }
 
     }
 }
