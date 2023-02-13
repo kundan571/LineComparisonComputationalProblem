5cae91f76555b23615d3717a6aecd2f4ea0649ef
kundan571 <kundanchauhan1997@gmail.com>
2023-02-13 23:40:27 +0530

    Calculated cartesian length

--- src/linecomparisonproblem/Linecomparisonucbuilder.java
+++ src/linecomparisonproblem/Linecomparisonucbuilder.java
@@ -1,7 +1,24 @@
 package linecomparisonproblem;
 
+import java.util.Scanner;
 public class Linecomparisonucbuilder {
+    // Calculate length and return
+    public int lengthCalculate(int x1, int y1, int x2, int y2) {
+        int length = (int) Math.pow((x2 - x1), 2) + (int) Math.pow((y2 - y1), 2);
+        return (int) Math.sqrt(length);
+    }
     public static void main(String[] args) {
-        System.out.println("Welcome to Line Comparison Computation Program");
+        Scanner input = new Scanner(System.in);
+        Linecomparisonucbuilder length = new Linecomparisonucbuilder();
+        int x1, y1, x2, y2;
+        System.out.println("Enter first coordinates x1 and y1: ");
+        x1 = input.nextInt();
+        y1 = input.nextInt();
+        System.out.println("Enter second coordinates x2 and y2: ");
+        x2 = input.nextInt();
+        y2 = input.nextInt();
+        System.out.println("length of line: " + length.lengthCalculate(x1, y1, x2, y2));
+
+
     }
 }
